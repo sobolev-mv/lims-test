906d1fb962fea4a510044a7ae08f7c24d1283ff8
sobolev_mv <sobolev_mv@nlmk.com>
2021-08-22 21:05:28 +0500

    Commit 1

--- Viz.WrkModule.MagLab/ViewModel/ViewModelMagLab.cs
+++ Viz.WrkModule.MagLab/ViewModel/ViewModelMagLab.cs
@@ -35,6 +35,7 @@ namespace Viz.WrkModule.MagLab
     private int currentMeasureUnit = 1;
     private int selectedMeasureDevice;
 
+    private Int32 Qm;
     private Int32 isQm;
     private string currentSampleId = null;
     private string currentTstep = null;
